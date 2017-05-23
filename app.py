4a0a8c776843063630395b7a40d9c8210168dea5
pengfei <pengfei@yunshan.net.cn>
2017-05-23 12:54:17 +0800

    add wechat send

--- app.py
+++ app.py
@@ -1,58 +1,32 @@
-import requests
-import ast
-import json
-from config import wechat_conf
-import simplejson
-class WeChat(object):
+# encoding=utf-8
+import time
+import spider
+from wechat import WeChat
 
-    def __init__(self):
-        self.corpid = wechat_conf["corpid"]
-        self.corpsecret = wechat_conf["corpsecret"]
-        self.party = wechat_conf["party"]
-        self.agentid = wechat_conf["agentid"]
-        self.token_url = wechat_conf["token_url"]
-        self.msg_url = wechat_conf["msg_url"]
+def main():
+    wc = WeChat()
+    spider_man = {
+        "hc3i": "",
+        "iyiou": "",
+        "sina": "",
+        "hchina": "",
+        "vcbeat": "",
+        "ckxx": "",
+        "xinhua": "",
+        "biodiscover": "",
+    }
+    while True:
+        for item in spider_man:
+            spider_res = getattr(spider, item)()
+            if spider_man[item] and spider_man[item] != spider_res["title"]:
+                print "new post"
+                wc.send(spider_res['title'], spider_res['preview'],
+                        spider_res['url'])
+            spider_man[item] = spider_res["title"]
 
-    def send(self, subject, msg):
-        msg = self.normalize(msg)
-        payload = {
-            "touser": "",
-            "toparty": self.party,
-            "msgtype": "news",
-            "agentid": self.agentid,
-            "news": {
-                "articles":
-                    [
-                        {
-                            "title": subject, 
-                            "description": msg, 
-                            "url": "",
-                            "picurl": ""
-                        }
-                    ]
-                },
-            "safe": "0"
-        }
-        
-        r = requests.post(self.msg_url + "?access_token=" + self.get_token(),
-                          data=simplejson.dumps(payload, ensure_ascii=False).encode('utf8'),
-                          headers={'Content-type':'application/json', 'charset': 'utf-8'})
-        return r
-    
-    def get_token(self):
-        payload = {
-            "corpid": self.corpid,
-            "corpsecret": self.corpsecret
-        }
-        r = requests.get(self.token_url, params=payload)
-        data = ast.literal_eval(r.text)
-        return data["access_token"]
-
-    def normalize(self, broker_message):
-        msg = broker_message
-        return msg
+        time.sleep(30)
+        print "another loop"
 
 
-wc = WeChat()
-
-wc.send('hello','world')
+if __name__ == '__main__':
+    main()
--- spider.py
+++ spider.py
@@ -1,7 +1,5 @@
 # encoding= utf-8
 from pyquery import PyQuery as pq
-from time import gmtime, strftime
-import re
 
 def hc3i():
     """中国数字医疗网"""
@@ -10,7 +8,7 @@ def hc3i():
     preview = element(".wrap .con").eq(0).text()
     title = element(".wrap .name a").eq(0).text()
     url = element(".wrap .name a").attr('href')
-    
+
     return {
         "title": title,
         "preview": preview,
@@ -33,7 +31,7 @@ def iyiou():
 
 def sina():
     """新浪医药"""
-    d = pq(url="http://med.sina.com/", encoding="utf-8") 
+    d = pq(url="http://med.sina.com/", encoding="utf-8")
     li = d(".news_1List li").eq(0)
     a = li("a").eq(0)
     url = a.attr('href')
@@ -47,7 +45,7 @@ def sina():
 
 def hchina():
     """健康中国"""
-    d = pq(url="http://health.china.com.cn/", encoding="utf-8") 
+    d = pq(url="http://health.china.com.cn/", encoding="utf-8")
     li = d(".rzuo .d1").eq(0)
     a = li("a").eq(0)
     url = a.attr('href')
@@ -61,7 +59,7 @@ def hchina():
 
 def vcbeat():
     """动脉网"""
-    d = pq(url="http://vcbeat.net/", encoding="utf-8") 
+    d = pq(url="http://vcbeat.net/", encoding="utf-8")
     li = d(".content li").eq(0)
     a = li("a").eq(1)
     url = a.attr('href')
@@ -82,9 +80,40 @@ def ckxx():
     url = a.attr('href')
     title = a.text()
     preview = title
-    
+
     return {
         "title": title,
         "preview": preview,
         "url": url,
+    }
+
+def xinhua():
+    """新华健康"""
+    d = pq(url="http://www.news.cn/health/", encoding="utf-8")
+    news = d(".headNews").eq(0)
+    a = news("a").eq(0)
+
+    url = a.attr('href')
+    title = a.text()
+    preview = news('.ywzy').text()
+
+    return {
+        "title": title,
+        "preview": preview,
+        "url": url,
+    }
+def biodiscover():
+    """生物探索"""
+    d = pq(url="http://www.biodiscover.com/news.html", encoding="utf-8")
+    news = d(".list_info .list_div").eq(0)
+    a = news("h1 a").eq(0)
+
+    url = a.attr('href')
+    title = a.text()
+    preview = news('.intro').text()
+
+    return {
+        "title": title,
+        "preview": preview,
+        "url": "http://www.biodiscover.com" + url,
     }
