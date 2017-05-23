bcd5ca2f3e29b82bbf723fb535ca6d1300a8934a
flyyang <pengfei_yang@outlook.com>
2017-05-23 22:30:42 +0800

    fix issue 3 title with website name

--- app.py
+++ app.py
@@ -15,17 +15,28 @@ def main():
         "xinhua": "",
         "biodiscover": "",
     }
+    site_map = {
+        "hc3i": "中国数字医疗网",
+        "iyiou": "亿欧",
+        "sina": "新浪医药",
+        "hchina": "健康中国",
+        "vcbeat": "动脉网",
+        "ckxx": "参考消息",
+        "xinhua": "新华健康",
+        "biodiscover": "生物探索",
+    }
     while True:
         for item in spider_man:
             spider_res = getattr(spider, item)()
             if spider_man[item] and spider_man[item] != spider_res["title"]:
                 print "new post"
-                wc.send(spider_res['title'], spider_res['preview'],
+                wc.send('【' + site_map[item] + '】' + spider_res['title'],
+                        spider_res['preview'],
                         spider_res['url'])
             spider_man[item] = spider_res["title"]
 
         time.sleep(30)
-        print "another loop"
+        print "new loop"
 
 
 if __name__ == '__main__':
