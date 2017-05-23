1d9dc03e77210b01064d71f4b7e718e00491ae89
pengfei <pengfei_yang@outlook.com>
2017-05-23 14:35:02 +0800

    update sina parser

--- spider.py
+++ spider.py
@@ -33,7 +33,7 @@ def sina():
     """新浪医药"""
     d = pq(url="http://med.sina.com/", encoding="utf-8")
     li = d(".news_1List li").eq(0)
-    a = li("a").eq(0)
+    a = li("a").eq(1)
     url = a.attr('href')
     title = a.text()
     preview = li(".indextext-ms").text()
